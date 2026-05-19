b8c38de9ab36eec5d6175258cdbf9833262569a6
Pedro Chaidez <149631039+Pedro-Chaidez@users.noreply.github.com>
2026-05-18 22:07:45 -0700

    Made sure door had the mask interactable

--- Assets/Scripts/House_To_Store.cs
+++ Assets/Scripts/House_To_Store.cs
@@ -3,6 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class DoorTeleport : Interactable
 {
+    private void Awake()
+    {
+        // Get the integer ID of the layer
+        int interactableLayer = LayerMask.NameToLayer("Interactable");
+
+        // Enforce the requirement: Check if the layer matches
+        if (gameObject.layer != interactableLayer)
+        {
+            Debug.LogWarning($"[{gameObject.name}] was not on the 'Interactable' layer. Setting it automatically.");
+            gameObject.layer = interactableLayer;
+        }
+    }
+
     protected override void Interact()
     {
         SceneManager.LoadScene("Store");
