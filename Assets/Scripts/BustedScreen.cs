d7d9cfacc20bdbfb566487861f4471c70aa77174
Hishaamx2 <156397947+Hishaamx2@users.noreply.github.com>
2026-06-11 18:53:20 -0700

    Added police system and busted screen to all weekday scenes

--- Assets/Scripts/BustedScreen.cs
+++ Assets/Scripts/BustedScreen.cs
@@ -2,68 +2,56 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
 using TMPro;
- 
+
 public class BustedScreen : MonoBehaviour
 {
     [Header("UI References")]
     public GameObject bustedPanel;
     public TextMeshProUGUI bustedText;
-    public UnityEngine.UI.Button restartButton;
     public UnityEngine.UI.Button mainMenuButton;
- 
+
     [Header("Settings")]
     public float showDelay = 0.5f;
     public string mainMenuSceneName = "Main Menu";
- 
+
     private bool hasBeenBusted = false;
- 
+    private InputManager inputManager;
+
     private void Start()
     {
-        if (bustedPanel != null)
-        {
-            bustedPanel.SetActive(false);
-        }
- 
-        if (restartButton != null)
-        {
-            restartButton.onClick.AddListener(RestartLevel);
-        }
- 
+        inputManager = FindAnyObjectByType<InputManager>();
+
+        if (mainMenuButton == null)
+            mainMenuButton = bustedPanel.transform.Find("MainMenuButton")
+                .GetComponent<UnityEngine.UI.Button>();
+
         if (mainMenuButton != null)
-        {
             mainMenuButton.onClick.AddListener(GoToMainMenu);
-        }
+
+        if (bustedPanel != null)
+            bustedPanel.SetActive(false);
     }
- 
+
     public void ShowBusted()
     {
         if (hasBeenBusted) return;
         hasBeenBusted = true;
- 
         StartCoroutine(ShowBustedRoutine());
     }
- 
+
     private IEnumerator ShowBustedRoutine()
     {
         Time.timeScale = 0f;
- 
+        if (inputManager != null) inputManager.SetPlayerControls(false);
+
         yield return new WaitForSecondsRealtime(showDelay);
- 
+
         if (bustedPanel != null)
-        {
             bustedPanel.SetActive(true);
-        }
- 
+
         Debug.Log("BUSTED!");
     }
- 
-    private void RestartLevel()
-    {
-        Time.timeScale = 1f;
-        hasBeenBusted = false;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
- 
+
     private void GoToMainMenu()
     {
         Time.timeScale = 1f;
